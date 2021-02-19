8926f70549c112eca97a7edefaa79a2ca9d03601
chideraony <67485765+chideraony@users.noreply.github.com>
2021-02-19 14:00:06 +0000

    created custom hook to get user location and current weather of the user's current location

--- src/currentLocation.js
+++ src/currentLocation.js
@@ -1,38 +1,55 @@
-import React, { Component } from 'react';
+const getCurrentLocation = () => {
+  const [location, setLocation] = useState({
+    accessGranted: false
+    coordinates: {latitude: "", longitude: ""}
+  });
 
-class App extends React.Component {
-  //state
-  state = {
-    userPosition: {
-      latitude: {},
-      longitude: {}
-    },
-    data: [],
-    dailyData: []
+  const onSuccess = location => {
+    setLocation({
+      accessGranted: true,
+      coordinates: {
+        latitude: location.coordinates.latitude,
+        longitude: location.coordinates.longitude,
+      }
+    })
   };
 
-  componentDidMount() {
-    //to check whether geolocation is supported
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        //to get the lat and long of user device
-        let pos = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        };
+  const onError = error => {
+    setLocation({
+      accessGranted: true,
+      error,
+    })
+  };
 
-        this.setState({ userPosition: pos });
-        fetch(
-          `https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.userPosition.latitude}&lon=${this.state.userPosition.longitude}&exclude=minutely&appid=ed58f59278aa5bb216a9921e812b2ac6`
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            this.setState({ data: data, dailyData: data.daily });
-          });
-      });
-    }
+  useEffect(() => {
+    if (!("geolocation" in navigator)) {
+      onError({
+        code: 0,
+        message: "Geolocation is not enabled on this browser",
+      })
+    },
+
+      navigator.geolocation.getCurrentPosition(onSuccess, onError)  
+      
+  }, [])
+
+  const getCurrentWeather = async (latitude, longitude) => {
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.userPosition.latitude}&lon=${this.state.userPosition.longitude}&exclude=minutely&appid=ed58f59278aa5bb216a9921e812b2ac6`
+    );
+    const data = await api_call.json();
+    this.setState({
+      latitude: lat,
+      longitude: lon,
+      current_weather: {
+        time: current.dt,
+        temperatureC: Math.round(current.temp),
+        weather_description: current.weather.id,
+      },
+    })
+    return getCurrentWeather()
   }
+      
+}
 
-  
-
-ReactDOM.render(<App/>, document.getElementById("root"));
+return location;
